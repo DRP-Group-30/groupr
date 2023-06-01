51071e661f17b8675175d11eb7cf8e8d53b8a4fe
Nathaniel Burke <nrb21@ic.ac.uk>
2023-06-01 22:50:38 +0100

    Add simple Firestore test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import { doc, getDoc } from "@firebase/firestore";
+import { db } from "./Firebase";
+import { DEFAULT_USER_ID, USERS } from "./Card";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test(`connect to firestore successfully`, async () => {
+  const snapshot = await getDoc(doc(db, USERS, DEFAULT_USER_ID));
+  expect(snapshot.get("Name")).toBe("DefaultUser");
 });
