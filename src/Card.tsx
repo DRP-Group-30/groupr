e6c5151dd8df309b92eaa88a8b5703dd3ae3ba14
Nathaniel Burke <nrb21@ic.ac.uk>
2023-06-01 16:19:12 +0100

    Add github action to run tests

--- src/Card.tsx
+++ src/Card.tsx
@@ -1,37 +1,40 @@
-import React from 'react';
-import { MouseEvent } from 'react';
+import React from "react";
+import { MouseEvent } from "react";
 
 const Card = () => {
-    const [offset, setOffset] = React.useState(0);
-    const [dragging, setDragging] = React.useState(false);
+  const [offset, setOffset] = React.useState(0);
+  const [dragging, setDragging] = React.useState(false);
 
-    function dragStart(ev: MouseEvent<HTMLDivElement>) {
-        setDragging(true);
-        setOffset(ev.pageX - window.innerWidth / 2);
-    }
+  function dragStart(ev: MouseEvent<HTMLDivElement>) {
+    setDragging(true);
+    setOffset(ev.pageX - window.innerWidth / 2);
+  }
 
-    function dragEnd(ev: MouseEvent<HTMLDivElement>) {
-        setDragging(false);
-        setOffset(0);
-    }
+  function dragEnd(ev: MouseEvent<HTMLDivElement>) {
+    setDragging(false);
+    setOffset(0);
+  }
 
-    function dragMove(ev: MouseEvent<HTMLDivElement>) {
-        if (!dragging) return;
+  function dragMove(ev: MouseEvent<HTMLDivElement>) {
+    if (!dragging) return;
 
-        setOffset(ev.pageX - window.innerWidth / 2);
-    }
+    setOffset(ev.pageX - window.innerWidth / 2);
+  }
 
-    return <div
-        className={`Card ${dragging ? "" : "Released"}`}
-        onMouseDown={dragStart}
-        onMouseUp={dragEnd}
-        onMouseMove={dragMove}
-        style={{
-            transform: `translate(${offset}px, 0) rotate(${offset / 10}deg)`
-        }}
+  return (
+    <div
+      className={`Card ${dragging ? "" : "Released"}`}
+      onMouseDown={dragStart}
+      onMouseUp={dragEnd}
+      onMouseMove={dragMove}
+      onMouseLeave={dragEnd}
+      style={{
+        transform: `translate(${offset}px, 0) rotate(${offset / 10}deg)`,
+      }}
     >
-        <h1>Nathaniel B</h1>
+      <h1>Nathaniel B's Gremlins</h1>
     </div>
-}
+  );
+};
 
 export default Card;
