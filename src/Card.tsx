61a01b60aa3c6bfe82acb1b0a3e56d318a142741
Nathaniel Burke <nrb21@ic.ac.uk>
2023-06-01 17:44:16 +0100

    Implement card fetching/replacement

--- src/Card.tsx
+++ src/Card.tsx
@@ -1,17 +1,75 @@
+import { rejects } from "assert";
 import React from "react";
 import { MouseEvent } from "react";
+import { db } from "./Firebase";
+import {
+  DocumentReference,
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+} from "firebase/firestore";
+
+const OFFSET_THRESHOLD = 300;
+const DEFAULT_USER_ID = "4yVmpEgdaQvizc3sNgas";
 
 const Card = () => {
   const [offset, setOffset] = React.useState(0);
   const [dragging, setDragging] = React.useState(false);
+  const [name, setName] = React.useState("");
+  let [cards, setCards] = React.useState<string[]>([]);
+  if (name === "") nextCard();
+
+  async function nextCard() {
+    if (cards.length === 0) await pollCards();
+    setName(inlineLog(cards[cards.length - 1]));
+    cards.pop();
+  }
+  async function pollCards() {
+    const defaultUser = await getDoc(doc(db, "users", DEFAULT_USER_ID));
+    const interested: DocumentReference[] = defaultUser.get("Interested");
+    const matched: DocumentReference[] = defaultUser.get("Matched");
+    const rejected: DocumentReference[] = defaultUser.get("Rejected");
+    const seenBefore = interested.concat(matched).concat(rejected);
+    const ds = await getDocs(collection(db, "projects"));
+
+    cards = ds.docs
+      .filter((d) => seenBefore.find((r) => r === d.ref) === undefined)
+      .map((n) => n.get("Name"));
+    setCards(cards);
+  }
 
   function dragStart(ev: MouseEvent<HTMLDivElement>) {
     setDragging(true);
     setOffset(ev.pageX - window.innerWidth / 2);
   }
 
-  function dragEnd(ev: MouseEvent<HTMLDivElement>) {
+  function acceptCard() {
+    console.log("Accept");
+  }
+
+  function rejectCard() {
+    console.log("Reject");
+  }
+
+  async function dragEnd(ev: MouseEvent<HTMLDivElement>) {
     setDragging(false);
+    if (offset > OFFSET_THRESHOLD) {
+    } else if (offset < -OFFSET_THRESHOLD) {
+    } else {
+    }
+
+    if (Math.abs(offset) >= OFFSET_THRESHOLD) {
+      if (offset > 0) {
+        acceptCard();
+      } else {
+        rejectCard();
+      }
+      // In an ideal world, we would have the card that flies up be a separate
+      // card, given the same state as the current one
+      nextCard();
+    }
+
     setOffset(0);
   }
 
@@ -22,19 +80,30 @@ const Card = () => {
   }
 
   return (
-    <div
-      className={`Card ${dragging ? "" : "Released"}`}
-      onMouseDown={dragStart}
-      onMouseUp={dragEnd}
-      onMouseMove={dragMove}
-      onMouseLeave={dragEnd}
-      style={{
-        transform: `translate(${offset}px, 0) rotate(${offset / 10}deg)`,
-      }}
-    >
-      <h1>Nathaniel B's Gremlins</h1>
+    <div>
+      <div
+        className={`Card ${dragging ? "" : "Released"}`}
+        onMouseDown={dragStart}
+        onMouseUp={dragEnd}
+        onMouseMove={dragMove}
+        onMouseLeave={dragEnd}
+        style={{
+          transform: `translate(${offset}px, 0) rotate(${offset / 10}deg)`,
+        }}
+      >
+        <h1>{name}</h1>
+      </div>
     </div>
   );
 };
 
 export default Card;
+
+function timeout(delay: number) {
+  return new Promise((res) => setTimeout(res, delay));
+}
+
+function inlineLog<T>(x: T): T {
+  console.log(x);
+  return x;
+}
---
+++ src/Firebase.ts
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore"
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDXfjUuI5_Zkx_aHnG3K83Hr24_Kd4HKtA",
+    authDomain: "drp-group-30.firebaseapp.com",
+    databaseURL:
+      "https://drp-group-30-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "drp-group-30",
+    storageBucket: "drp-group-30.appspot.com",
+    messagingSenderId: "812261315433",
+    appId: "1:812261315433:web:baacf1b49b8bdce34ee3f8",
+  };
+
+initializeApp(firebaseConfig);
+export const db = getFirestore();
