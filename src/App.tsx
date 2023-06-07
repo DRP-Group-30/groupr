6061da721a28ef44acd0e9d33fbf0804cbc46eba
Pratyaksh Sharma <pratyaksh.sharma21@imperial.ac.uk>
2023-06-07 12:46:50 +0100

    Include Chakra navigation bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import Dashboard from "./Dashboard";
+import Navbar from "./Navbar";
 import Finder from "./Finder";
 
 function App() {
   return (
     <>
+      <Navbar></Navbar>
       <Routes>
         <Route path="/" element={<Finder />} />
         <Route path="/dashboard" element={<Dashboard />} />
--- src/Finder.tsx
+++ src/Finder.tsx
@@ -1,8 +1,9 @@
 import { DocumentReference, DocumentData, getDoc, updateDoc } from 'firebase/firestore';
 import './App.css';
-import Card from './Card';
+// import Card from './Card';
 import { Link } from "react-router-dom";
 import { DEFAULT_USER, USER_CARD_CATEGORIES } from './Card';
+import SwipeCard from "./SwipeCard";
 
 const Finder = () => {
     async function resetLists() {
@@ -12,21 +13,24 @@ const Finder = () => {
         ).then(() => window.location.reload());
     }
 
-    return <div className="App">
-        <Link to='/dashboard'>Dashboard</Link>
-        <Card></Card>
+    return (
+      <div className="App">
+        <Link to="/dashboard">Dashboard</Link>
+        {/* <Card></Card> */}
+        <SwipeCard></SwipeCard>
         <button onClick={resetLists}>Reset</button>
-    </div >
+      </div>
+    );
 }
 
-const updateFields = (
-    d: DocumentReference<DocumentData>,
-    fs: [string, (x: any) => any][]
-): Promise<void> =>
-    getDoc(d)
-        .then((snapshot) => fs.map(([f, _]) => snapshot.get(f)))
-        .then((n) =>
-            updateDoc(d, Object.fromEntries(fs.map(([f, m], i) => [f, m(n[i])])))
-        );
-
 export default Finder;
+
+const updateFields = (
+  d: DocumentReference<DocumentData>,
+  fs: [string, (x: any) => any][]
+): Promise<void> =>
+  getDoc(d)
+    .then((snapshot) => fs.map(([f, _]) => snapshot.get(f)))
+    .then((n) =>
+      updateDoc(d, Object.fromEntries(fs.map(([f, m], i) => [f, m(n[i])])))
+    );
