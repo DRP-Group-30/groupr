4276577e21ca045f60ff3938aa2e032c25c2e661
Pratyaksh Sharma <pratyaksh.sharma21@imperial.ac.uk>
2023-06-07 17:35:29 +0100

    Refactor card swiping to use props

--- src/Finder.tsx
+++ src/Finder.tsx
@@ -6,58 +6,110 @@ import {
 } from "firebase/firestore";
 import "./App.css";
 // import Card from './Card';
-import { Link } from "react-router-dom";
 import { DEFAULT_USER, USER_CARD_CATEGORIES } from "./Card";
 import { MdDone, MdClose } from "react-icons/md";
 import SwipeCard from "./SwipeCard";
-import { Button, Center, Flex, Spacer } from "@chakra-ui/react";
-import { useState } from "react";
+import { Button, Center, Flex, Grid, GridItem } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 
 const Finder = () => {
-  const [dragging, setDragging] = useState(false);
+	const [offset, setOffset] = useState(0);
+	const [dragging, setDragging] = useState(false);
+	let [sideBarWidth, setSideBarWidth] = useState(25);
+	const [cardAnchor, setCardAnchor] = useState(0);
 
-  async function resetLists() {
-    updateFields(
-      DEFAULT_USER,
-      USER_CARD_CATEGORIES.map((c) => [c, () => []])
-    ).then(() => window.location.reload());
-  }
+	useEffect(() => {
+		setCardAnchor(
+			window.innerWidth * (sideBarWidth / 100) +
+				(window.innerWidth * (1 - sideBarWidth / 100)) / 2,
+		);
+	});
 
-  function dragStart() {
-    setDragging(true);
-  }
+	async function resetLists() {
+		updateFields(
+			DEFAULT_USER,
+			USER_CARD_CATEGORIES.map(c => [c, () => []]),
+		).then(() => window.location.reload());
+	}
 
-  function dragEnd() {
-    setDragging(false);
-  }
+	function toggleSideBar() {
+		setSideBarWidth((sideBarWidth = sideBarWidth === 4 ? 25 : 4));
+		setCardAnchor(
+			window.innerWidth * (sideBarWidth / 100) +
+				(window.innerWidth * (1 - sideBarWidth / 100)) / 2,
+		);
+	}
 
-  return (
-    <Center h="100%">
-      <Flex justifyContent="space-evenly" alignItems="center" w="60%">
-        <Button
-          className={`${dragging ? "Hidden" : ""}`}
-          leftIcon={<MdClose />}
-        >
-          {" "}
-          Reject{" "}
-        </Button>
-        <SwipeCard dragStart={dragStart} dragEnd={dragEnd}></SwipeCard>
-        <Button
-          className={`${dragging ? "Hidden" : ""}`}
-          rightIcon={<MdDone />}
-        >
-          {" "}
-          Accept
-        </Button>
-      </Flex>
-    </Center>
+	function acceptCard() {
+		setOffset(window.innerWidth / 2);
+	}
 
-    // <div className="App">
-    //   {/* <Card></Card> */}
-    //   <SwipeCard></SwipeCard>
-    //   <button onClick={resetLists}>Reset</button>
-    // </div>
-  );
+	function rejectCard() {
+		setOffset(-window.innerWidth / 2);
+	}
+
+	return (
+		<Grid
+			templateAreas={`"nav main"`}
+			gridTemplateRows={"100% 1fr"}
+			gridTemplateColumns={`${sideBarWidth}% 1fr`}
+			height="calc(100% - 64px)"
+			color="blackAlpha.700"
+			fontWeight="bold"
+			style={{
+				transition: "all 0.5s",
+			}}
+		>
+			<GridItem pl="2" bg="gray.100" area={"nav"} zIndex="9999">
+				<Flex h="100%" flexDirection="column" justifyContent="center" alignItems="center">
+					<Button
+						onClick={toggleSideBar}
+						alignSelf="flex-end"
+						transform="translate(50%)"
+						bg="white"
+						boxShadow={"base"}
+					>
+						{">"}
+					</Button>
+				</Flex>
+			</GridItem>
+			<GridItem pl="2" area={"main"}>
+				<Center h="100%">
+					<Flex justifyContent="space-evenly" alignItems="center" w="60%">
+						<Button
+							className={`${dragging ? "Hidden" : ""}`}
+							onClick={dragging ? rejectCard : () => {}}
+							leftIcon={<MdClose />}
+						>
+							Reject
+						</Button>
+						<SwipeCard
+							offset={offset}
+							setOffset={setOffset}
+							dragging={dragging}
+							setDragging={setDragging}
+							cardAnchor={cardAnchor}
+							acceptCard={acceptCard}
+							rejectCard={rejectCard}
+						></SwipeCard>
+						<Button
+							className={`${dragging ? "Hidden" : ""}`}
+							onClick={dragging ? acceptCard : () => {}}
+							rightIcon={<MdDone />}
+						>
+							Accept
+						</Button>
+					</Flex>
+				</Center>
+			</GridItem>
+		</Grid>
+
+		// <div className="App">
+		//   {/* <Card></Card> */}
+		//   <SwipeCard></SwipeCard>
+		//   <button onClick={resetLists}>Reset</button>
+		// </div>
+	);
 };
 
 export default Finder;
--- src/SwipeCard.tsx
+++ src/SwipeCard.tsx
@@ -8,122 +8,118 @@ import {
   Avatar,
   useColorModeValue,
 } from "@chakra-ui/react";
-import { useState, MouseEvent } from "react";
+import { useState, MouseEvent, Dispatch, SetStateAction } from "react";
 
 interface SwipeCardProps {
-  dragStart: () => void;
-  dragEnd: () => void;
+	offset: number;
+	setOffset: Dispatch<SetStateAction<number>>;
+	dragging: boolean;
+	setDragging: Dispatch<SetStateAction<boolean>>;
+	cardAnchor: number;
+	acceptCard: () => void;
+	rejectCard: () => void;
 }
 
-const SwipeCard = (props: SwipeCardProps) => {
-  const [offset, setOffset] = useState(0);
-  const [dragging, setDragging] = useState(false);
+const SwipeCard = ({
+	offset,
+	setOffset,
+	dragging,
+	setDragging,
+	cardAnchor,
+	acceptCard,
+	rejectCard,
+}: SwipeCardProps) => {
+	function dragStart(ev: MouseEvent<HTMLDivElement>) {
+		setDragging(true);
+		setOffset(ev.pageX - cardAnchor);
+	}
 
-  function dragStart(ev: MouseEvent<HTMLDivElement>) {
-    setDragging(true);
-    setOffset(ev.pageX - window.innerWidth / 2);
-    props.dragStart();
-  }
+	function dragEnd(ev: MouseEvent<HTMLDivElement>) {
+		setDragging(false);
 
-  function dragEnd(ev: MouseEvent<HTMLDivElement>) {
-    setDragging(false);
+		if (Math.abs(offset) >= 200) {
+			if (offset > 0) {
+				acceptCard();
+			} else {
+				rejectCard();
+			}
+			// In an ideal world, we would have the card that flies up be a separate
+			// card, given the same state as the current one
+			// nextCard();
+		} else {
+			setOffset(0);
+		}
+	}
 
-    // if (Math.abs(offset) >= OFFSET_THRESHOLD) {
-    //   if (offset > 0) {
-    //     acceptCard();
-    //   } else {
-    //     rejectCard();
-    //   }
-    //   // In an ideal world, we would have the card that flies up be a separate
-    //   // card, given the same state as the current one
-    //   nextCard();
-    // }
+	function dragMove(ev: MouseEvent<HTMLDivElement>) {
+		if (!dragging) return;
 
-    setOffset(0);
-    props.dragEnd();
-  }
+		setOffset(ev.pageX - cardAnchor);
+	}
 
-  function dragMove(ev: MouseEvent<HTMLDivElement>) {
-    if (!dragging) return;
-
-    setOffset(ev.pageX - window.innerWidth / 2);
-  }
-
-  return (
-    <Center py={6}>
-      <Box
-        maxW={"445px"}
-        w={"full"}
-        bg={useColorModeValue("white", "gray.900")}
-        boxShadow={"2xl"}
-        rounded={"md"}
-        p={6}
-        overflow={"hidden"}
-        className={`${dragging ? "" : "Released"}`}
-        onMouseDown={dragStart}
-        onMouseUp={dragEnd}
-        onMouseMove={dragMove}
-        onMouseLeave={dragEnd}
-        style={{
-          transform: `translate(${offset}px, 0) rotate(${offset / 15}deg)`,
-          userSelect: "none",
-          zIndex: "9999",
-          cursor: "pointer",
-        }}
-      >
-        <Box
-          h={"210px"}
-          bg={"gray.100"}
-          mt={-6}
-          mx={-6}
-          mb={6}
-          pos={"relative"}
-        >
-          <Image
-            src={
-              "https://images.unsplash.com/photo-1515378791036-0648a3ef77b2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
-            }
-            boxSize="100%"
-            objectFit="fill"
-            draggable="false"
-          />
-        </Box>
-        <Stack>
-          <Text
-            color={"green.500"}
-            textTransform={"uppercase"}
-            fontWeight={800}
-            fontSize={"sm"}
-            letterSpacing={1.1}
-          >
-            Blog
-          </Text>
-          <Heading
-            color={useColorModeValue("gray.700", "white")}
-            fontSize={"2xl"}
-            fontFamily={"body"}
-          >
-            Boost your conversion rate
-          </Heading>
-          <Text color={"gray.500"}>
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
-            et ea rebum.
-          </Text>
-        </Stack>
-        <Stack mt={6} direction={"row"} spacing={4} align={"center"}>
-          <Avatar
-            src={"https://avatars0.githubusercontent.com/u/1164541?v=4"}
-          />
-          <Stack direction={"column"} spacing={0} fontSize={"sm"}>
-            <Text fontWeight={600}>Achim Rolle</Text>
-            <Text color={"gray.500"}>Feb 08, 2021 · 6min read</Text>
-          </Stack>
-        </Stack>
-      </Box>
-    </Center>
-  );
+	return (
+		<Center py={6}>
+			<Box
+				maxW={"445px"}
+				w={"full"}
+				bg={useColorModeValue("white", "gray.900")}
+				boxShadow={"2xl"}
+				rounded={"md"}
+				p={6}
+				overflow={"hidden"}
+				className={`${dragging ? "" : "Released"}`}
+				onMouseDown={dragStart}
+				onMouseUp={dragEnd}
+				onMouseMove={dragMove}
+				onMouseLeave={dragEnd}
+				style={{
+					transform: `translate(${offset}px, 0) rotate(${offset / 20}deg)`,
+					userSelect: "none",
+				}}
+			>
+				<Box h={"210px"} bg={"gray.100"} mt={-6} mx={-6} mb={6} pos={"relative"}>
+					<Image
+						src={
+							"https://images.unsplash.com/photo-1515378791036-0648a3ef77b2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
+						}
+						boxSize="100%"
+						objectFit="fill"
+						draggable="false"
+					/>
+				</Box>
+				<Stack>
+					<Text
+						color={"green.500"}
+						textTransform={"uppercase"}
+						fontWeight={800}
+						fontSize={"sm"}
+						letterSpacing={1.1}
+					>
+						Blog
+					</Text>
+					<Heading
+						color={useColorModeValue("gray.700", "white")}
+						fontSize={"2xl"}
+						fontFamily={"body"}
+					>
+						Boost your conversion rate
+					</Heading>
+					<Text color={"gray.500"}>
+						Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+						eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+						voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+					</Text>
+				</Stack>
+				<Stack mt={6} direction={"row"} spacing={4} align={"center"}>
+					<Avatar src={"https://avatars0.githubusercontent.com/u/1164541?v=4"} />
+					<Stack direction={"column"} spacing={0} fontSize={"sm"}>
+						<Text fontWeight={600}>Achim Rolle</Text>
+						<Text color={"gray.500"}>Feb 08, 2021 · 6min read</Text>
+					</Stack>
+				</Stack>
+			</Box>
+		</Center>
+	);
 };
 
 export default SwipeCard;
