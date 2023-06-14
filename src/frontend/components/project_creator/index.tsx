050d7ee5b1121db28b9826b00cd7510dc62b3329
Pratyaksh Sharma <pratyaksh.sharma21@imperial.ac.uk>
2023-06-14 19:00:48 +0100

    Add image upload on project creation

--- src/frontend/components/project_creator/index.tsx
+++ src/frontend/components/project_creator/index.tsx
@@ -8,10 +8,16 @@ import { Input } from "@chakra-ui/input";
 import { Checkbox } from "@chakra-ui/checkbox";
 import { Button } from "@chakra-ui/button";
 import { Select } from "@chakra-ui/select";
+import { Image } from "@chakra-ui/react";
+import { AddIcon } from "@chakra-ui/icons";
+import { MdUploadFile } from "react-icons/md";
 
 type Fields = "fields";
 
 const ProjectCreator = () => {
+	const [tempCoverImage, setTempCoverImage] = useState<File | null>(null);
+	const [imageInputElem, setImageInputElem] = useState<HTMLInputElement | null>(null);
+
 	const formik = useFormik<Project[Fields]>({
 		initialValues: {
 			name: "",
@@ -32,9 +38,41 @@ const ProjectCreator = () => {
 
 	return (
 		<Flex bg="gray.100" align="center" justify="center" h="100vh">
-			<Box bg="white" p={6} rounded="md">
+			<Box bg="white" minWidth="400px" p={6} rounded="md" overflow="hidden">
+				<Box backgroundColor="teal.400" height="210px" mt={-6} mx={-6} position="relative">
+					{tempCoverImage !== null && (
+						<Image
+							src={URL.createObjectURL(tempCoverImage)}
+							objectFit="fill"
+							boxSize="100%"
+						></Image>
+					)}
+				</Box>
 				<form onSubmit={formik.handleSubmit}>
 					<VStack spacing={4} align="flex-start">
+						<FormControl transform="translate(0, -50%)">
+							<Flex justifyContent="right" alignItems="center">
+								<input
+									id="cover_image"
+									name="coverImage"
+									type="file"
+									accept="image/*"
+									onChange={event => {
+										setTempCoverImage((event.currentTarget.files ?? [null])[0]);
+									}}
+									ref={input => setImageInputElem(input)}
+									hidden
+								/>
+								<Button
+									fontWeight="400"
+									leftIcon={<MdUploadFile />}
+									onClick={() => imageInputElem?.click()}
+									boxShadow="lg"
+								>
+									{tempCoverImage ? "Change cover image" : "Upload cover image"}
+								</Button>
+							</Flex>
+						</FormControl>
 						<FormControl>
 							<FormLabel htmlFor="project name">Project Name</FormLabel>
 							<Input
