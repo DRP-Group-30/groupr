ce4fa4da1f77d4bf117b2a5b1ff4c0b8abc946c6
Pratyaksh Sharma <pratyaksh.sharma21@imperial.ac.uk>
2023-06-15 22:51:15 +0100

    Add personal details from currentUser to profile

--- src/frontend/components/profile-page/index.tsx
+++ src/frontend/components/profile-page/index.tsx
@@ -18,98 +18,101 @@ import {
 	Button,
 	TagLeftIcon,
 	TagLabel,
+	Avatar,
 } from "@chakra-ui/react";
 
 import { MdAttachMoney } from "react-icons/md";
+import { useAuth } from "../../../context/AuthContext";
 
-const ProfileHeader = () => (
-	<Container bgColor="ffffff00" p={4} w="100%" alignItems="center" justifyContent="center">
-		<Flex
-			shadow="lg"
-			rounded="lg"
-			bg="#ffffff"
-			_dark={{
-				bg: "gray.800",
-			}}
-			mb={4}
-			direction="column"
-			alignItems="center"
-			justifyContent="center"
-		>
-			<Box
+const ProfileHeader = () => {
+	const { currentUser, logout } = useAuth();
+	const isLoggedIn = !!currentUser;
+
+	return (
+		<Container bgColor="ffffff00" p={4} w="100%" alignItems="center" justifyContent="center">
+			<Flex
+				width="100%"
+				shadow="lg"
+				rounded="lg"
 				bg="#ffffff"
 				_dark={{
-					bg: "#3e3e3e",
+					bg: "gray.800",
 				}}
-				style={{
-					backgroundImage:
-						"url(https://images.unsplash.com/photo-1666795599746-0f62dfa29a07?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80)",
-					backgroundSize: "cover",
-					backgroundPosition: "center",
-					backgroundRepeat: "no-repeat",
-				}}
-				height="100%"
-				width="100%"
-				borderRadius="lg"
-				p={8}
-				display="flex"
-				alignItems="left"
+				mb={4}
+				direction="column"
+				alignItems="center"
+				justifyContent="center"
 			>
-				<Image
-					src="https://images.unsplash.com/photo-1623930154261-37f8b293c059?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80"
-					alt="Profile Picture"
-					borderRadius="full"
-					boxSize="150px"
-					shadow="lg"
-					border="5px solid"
-					mb={-20}
-					borderColor="gray.800"
+				<Box
+					bg="#ffffff"
 					_dark={{
-						borderColor: "gray.200",
+						bg: "#3e3e3e",
 					}}
-				/>
-			</Box>
-			<Box
-				gridColumn="span 8"
-				p={8}
-				width="full"
-				height="full"
-				borderRadius="lg"
-				textAlign="left"
-				mt={10}
-			>
-				<Text
-					fontSize="4xl"
-					fontWeight="bold"
-					color="gray.800"
-					_dark={{
-						color: "white",
+					style={{
+						backgroundImage: `url(https://picsum.photos/seed/${currentUser?.uid}/1200)`,
+						backgroundSize: "cover",
+						backgroundPosition: "center",
+						backgroundRepeat: "no-repeat",
 					}}
+					height="100%"
+					width="100%"
+					roundedTop="lg"
+					px={8}
+					display="flex"
+					alignItems="left"
 				>
-					Christian Buehner
-				</Text>
-				<HStack
-					spacing={3}
-					color="gray.800"
-					_dark={{
-						color: "gray.200",
-					}}
+					<Avatar
+						src={currentUser?.photoURL ?? ""}
+						name={currentUser?.displayName ?? undefined}
+						borderRadius="full"
+						borderWidth="5px"
+						size="2xl"
+						shadow="lg"
+						transform="translate(0, 50%)"
+					/>
+				</Box>
+				<Box
+					gridColumn="span 8"
+					p={8}
+					width="full"
+					height="full"
+					borderRadius="lg"
+					textAlign="left"
+					mt={10}
 				>
 					<Text
-						fontSize="2xl"
+						fontSize="4xl"
 						fontWeight="bold"
+						color="gray.800"
+						_dark={{
+							color: "white",
+						}}
+					>
+						{currentUser?.displayName}
+					</Text>
+					<HStack
+						spacing={3}
 						color="gray.800"
 						_dark={{
 							color: "gray.200",
 						}}
 					>
-						He/Him
-					</Text>
-				</HStack>
-			</Box>
-		</Flex>
-	</Container>
-);
+						<Text
+							fontSize="2xl"
+							fontWeight="bold"
+							color="gray.800"
+							_dark={{
+								color: "gray.200",
+							}}
+						>
+							He/Him
+						</Text>
+					</HStack>
+				</Box>
+			</Flex>
+		</Container>
+	);
+};
 
 
 const ProfileSide = () => {
